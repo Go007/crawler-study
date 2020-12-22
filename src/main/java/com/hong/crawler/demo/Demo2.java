0f30faf9938dd580afaff2fc22b345cecf60d524
wanghong <wanghong@chinacsci.com>
2020-12-22 10:26:01 +0800

    demo2

--- src/main/java/com/hong/crawler/demo/Demo2.java
+++ src/main/java/com/hong/crawler/demo/Demo2.java
@@ -2,6 +2,7 @@ package com.hong.crawler.demo;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 /**
  * @author wanghong
@@ -12,7 +13,10 @@ public class Demo2 {
 
     public static void main(String[] args) throws Exception{
         String url = "http://221.214.94.51:8081/icity/ipro/projectlist";
-        Document document = Jsoup.connect(url).get();
-
+        Document doc = Jsoup.connect(url).userAgent("Mozilla").cookie("is_click", "1").timeout(10000).get();
+        Elements span = doc.select("#w-d-x-1").select("span[class=btn btn-sm btn-warning]");
+        System.out.println(span);
+        String onclick = span.attr("onclick");
+        System.out.println(onclick);
     }
 }
